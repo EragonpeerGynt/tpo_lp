3297f810757d6d638a60fa8a1a828da5332aa55f
Eragon_peerGynt <eragon.peergynt@gmail.com>
2019-05-10 18:05:21 +0200

    SSL dodan

--- src/server/server.py
+++ src/server/server.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_sslify import SSLify
 import index
 app = Flask(__name__.split(".")[0])
+
 
 #funkciji, ki se pokličeta ko uporabnik zahteva začetno stran aplikacije
 @app.route('/')
@@ -10,3 +12,7 @@ def slash():
 @app.route('/index.html')
 def slash_index():
     return index.main_page()
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=False, ssl_context='adhoc')
