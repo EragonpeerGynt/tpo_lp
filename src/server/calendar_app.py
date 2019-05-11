242833a104da22944e616eb76a42517aeffcd888
Eragon_peerGynt <eragon.peergynt@gmail.com>
2019-05-11 21:07:20 +0200

    Predpriprava koledarja na vnose ref #7

--- src/server/calendar_app.py
+++ src/server/calendar_app.py
@@ -1,6 +1,7 @@
-def main_page():
+def add():
     html_background = open("html_files/index.html")
-    html_file = html_background.read().replace("$_WINDOW_", "")
+    html_file = open("html_files/calendar_conf.html","r")
+    html_file = html_background.read().replace("<!--WINDOW-->", html_file.read())
     csser = open("static/style/styl.css", "r")
     html_file = html_file.replace("/*STYLE*/", csser.read())
     return html_file
