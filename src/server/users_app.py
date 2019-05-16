909be9c24d77569d819c7192c8c58980a0b2cfc6
Andreja Jerič <aj6009@student.uni-lj.si>
2019-05-16 09:21:05 +0000

    Popravljeni HTML in py datoteki za registracijo ref #2

--- src/server/users_app.py
+++ src/server/users_app.py
@@ -1,4 +1,6 @@
 from flask import *
+from flask_mail import Mail
+from flask_mail import Message
 import os
 import datetime
 from dbase import database
@@ -7,11 +9,17 @@ import logging
 
 app = Blueprint('users_app', __name__)
 
-@app.route('/register')
+@app.route('/register', methods=['POST'])
 def registracija():
+    if request.form['knof'] == "Prijavi se":
+        return redirect(url_for('users_app.vpis'))
     obvestilo=""
-    uporabnisko = request.form['uporabnisko']
+    uporabnisko = request.form['mail']
     geslo = request.form['geslo']
+    
+    #if geslo=="" or uporabnisko=="":
+     #   obvestilo="Prosim vpisite vse podatke."
+      #  return render_template("register.html", napaka=obvestilo)
     
     if len(geslo) < 8:
         obvestilo="Geslo je prekratko."
@@ -24,12 +32,18 @@ def registracija():
     cur.execute(table, (uporabnisko, ))
     records = cur.fetchone()
     
-    st_takih= records[0]
+    st_takih=1
+    
+    try:
+        st_takih= records[0]
+    except:
+        st_takih= 0
     
     #ce ze obstaja
     if st_takih != 0:
         obvestilo="Ta elektronski naslov je ze registriran."
         return render_template("register.html", napaka=obvestilo)
+        
         
     query = 'INSERT INTO `Uporabnik` (mail, geslo, status, potrjen) VALUES (%s, %s, %s, %s)'
     cur.execute(query, (uporabnisko, geslo, 0, 0))
@@ -38,7 +52,12 @@ def registracija():
     
     obvestilo="Na vnesen elektronski naslov smo poslali sporocilo za potrditev registracije. Registracijo morate potrditi v dveh dnevih."
     
-    return render_template("login.html", napaka=obvestilo)
+    return render_template("register.html", napaka=obvestilo)
+    
+@app.route('/register')
+def regist():
+    obvestilo=""
+    return render_template("register.html", napaka=obvestilo)
     
 @app.route('/logout')
 def izpis():
@@ -60,8 +79,12 @@ def vpis():
     cur.execute(table, (uporabnisko, ))
     records = cur.fetchone()
     
-    st_takih= records[0]
+    st_takih=1
     
+    try:
+        st_takih= records[0]
+    except:
+        st_takih= 0
     
     #ce ni takega
     if st_takih == 0:
