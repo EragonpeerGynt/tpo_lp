65a2cab2fa229c649e3b5f4d227f0b0fd64655b7
Eragon peerGynt <eragon.peergynt@gmail.com>
2019-05-23 09:29:24 +0000

    preizkus heroku

--- app.py
+++ app.py
@@ -1,16 +1,8 @@
-from flask import Flask
-from datetime import datetime
-app = Flask(__name__)
+import os
+import sys
+dirname, filename = os.path.split(os.path.abspath(__file__))
+serverdir = os.path.join(dirname, "src", "server")
+sys.path.append(serverdir)
+import server
 
-@app.route('/')
-def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
-
-    return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
-    <img src="http://loremflickr.com/600/400" />
-    """.format(time=the_time)
-
-if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+server.heroku_star()
