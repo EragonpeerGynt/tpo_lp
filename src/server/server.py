7535f45a0aee9ecb750294f9e32acc92e0f9eddd
Eragon peerGynt <eragon.peergynt@gmail.com>
2019-05-23 20:57:52 +0000

    reroll

--- src/server/server.py
+++ src/server/server.py
@@ -24,8 +24,8 @@ app.register_blueprint(users_app, url_prefix = '')
 def index():
     return render_template("index.html")
 
-#def heroku_start():
-#    app.run(debug=True, use_reloader=True)
+def heroku_start():
+    app.run(debug=True, use_reloader=True)
 
 if __name__ == '__main__':
 #    #localhost version
