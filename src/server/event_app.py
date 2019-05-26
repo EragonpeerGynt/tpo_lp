d7c0fdb1dd6fc22c57106f4958522e22818812b4
Eragon peerGynt <eragon.peergynt@gmail.com>
2019-05-26 22:24:11 +0000

    popravek koledarja ref #16

--- src/server/event_app.py
+++ src/server/event_app.py
@@ -15,6 +15,50 @@ except NameError:
 
 app = Blueprint('event_app', __name__)
 
+@app.route('/')
+def zacetna_stran():
+    try:
+        user_id = session['id_u']
+    except:
+        return redirect( url_for('index') )
+    
+        
+    db = database.dbcon()
+    cur = db.cursor()
+    query = 'SELECT status FROM `Uporabnik` WHERE id = %s'
+    dat = (user_id,)
+    cur.execute(query, dat)
+    dat = cur.fetchall()
+    cur.close()
+    
+    try:
+        status = dat[0]
+    except:
+        return redirect( url_for('index') )
+        
+    cur = db.cursor()
+    query = 'SELECT id, ime, datum FROM `dogodek`'
+    cur.execute(query, ())
+    prihajajoce = cur.fetchall()
+    cur.close()
+    
+    #current_app.logger.error(prihajajoce)
+    #current_app.logger.error(prihajajoce[0][2].strftime('%d. %m. %Y %H:%M'))
+    
+    if len(prihajajoce) != 0:
+        imena = ['id', 'naziv', 'datum']
+        #for i in range(len(prihajajoce)):
+        #    prihajajoce[i][2] = prihajajoce[i][2].strftime('%d. %m. %Y %H:%M')
+        future_cal = [ { imena[j]:prihajajoce[i][j] for j in range(3)} for i in range(len(prihajajoce)) ]
+        for i in range(len(future_cal)):
+            future_cal[i]['datum'] = future_cal[i]['datum'].strftime('%d. %m. %Y %H:%M')
+    else:
+        future_cal = []
+    
+    
+    current_app.logger.error(status)
+    return render_template("events.html", podatki_dogodki=future_cal, status=status)
+
 @app.route('/add')
 def vnesi_podatke_nov():
     try:
